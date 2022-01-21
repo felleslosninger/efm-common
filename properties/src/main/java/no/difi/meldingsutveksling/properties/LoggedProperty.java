b6da32dba36d8656c0c7aeee6621e7e293cb6622
Frode Bjerkholt <frode.bjerkholt@gmail.com>
2022-01-21 13:31:34 +0100

    MOVE-2694 Legg på @LoggedProperty for ny DPI

---
+++ properties/src/main/java/no/difi/meldingsutveksling/properties/LoggedProperty.java
@@ -0,0 +1,14 @@
+package no.difi.meldingsutveksling.properties;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+public @interface LoggedProperty {
+
+}
