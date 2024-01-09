37bde77a0fe79f087a29ae0edc7b663b3ffbab50
Håvard England <hme@digdir.no>
2024-01-09 10:37:59 +0100

    MOVE-3888 fix x509 badly formatted directory string

--- certvalidator/src/test/java/no/difi/certvalidator/rule/CriticalExtensionRequiredRuleTest.java
+++ certvalidator/src/test/java/no/difi/certvalidator/rule/CriticalExtensionRequiredRuleTest.java
@@ -3,11 +3,8 @@ package no.difi.certvalidator.rule;
 
 import no.difi.certvalidator.ValidatorBuilder;
 import no.difi.certvalidator.api.FailedValidationException;
-import no.difi.certvalidator.testutil.X509ExtensionCustom;
 import no.difi.certvalidator.testutil.X509TestGenerator;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import org.bouncycastle.cert.CertIOException;
-import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.testng.annotations.Test;
 
 import java.security.cert.X509Certificate;
@@ -24,12 +21,7 @@ public class CriticalExtensionRequiredRuleTest extends X509TestGenerator {
     @Test
     public void shouldValidateCertWithApprovedCriticalExtentions() throws Exception {
         CriticalExtensionRequiredRule validator = CriticalExtensionRule.requires("2.10.2");
-        X509Certificate cert = createX509Certificate(new X509ExtensionCustom() {
-            public void setup(X509v3CertificateBuilder v3CertGen) throws CertIOException {
-                v3CertGen.addExtension(new ASN1ObjectIdentifier("2.10.2"), true, new byte[3]);
-            }
-
-        });
+        X509Certificate cert = createX509Certificate(v3CertGen -> v3CertGen.addExtension(new ASN1ObjectIdentifier("2.10.2"), true, new byte[3]));
         validator.validate(cert);
     }
 
@@ -37,11 +29,9 @@ public class CriticalExtensionRequiredRuleTest extends X509TestGenerator {
     public void shouldInvalidateCertWithACriticalExtentionsThatIsNotApproved() throws Exception {
         String approvedExtentionList = "2.10.2";
         CriticalExtensionRequiredRule validator = CriticalExtensionRule.requires(approvedExtentionList);
-        X509Certificate cert = createX509Certificate(new X509ExtensionCustom() {
-            public void setup(X509v3CertificateBuilder v3CertGen) throws CertIOException {
-                String notApprovedExtention = "2.10.6";
-                v3CertGen.addExtension(new ASN1ObjectIdentifier(notApprovedExtention), true, new byte[3]);
-            }
+        X509Certificate cert = createX509Certificate(v3CertGen -> {
+            String notApprovedExtention = "2.10.6";
+            v3CertGen.addExtension(new ASN1ObjectIdentifier(notApprovedExtention), true, new byte[3]);
         });
         validator.validate(cert);
     }
