e5056107156f7fccf2e698a8e35ee836e2e34a02
Håvard England <hme@digdir.no>
2024-02-07 12:32:00 +0100

    MOVE-3888 Refactor code to handle exceptions with try catch

--- security/src/main/java/no/difi/move/common/cert/KeystoreProvider.java
+++ security/src/main/java/no/difi/move/common/cert/KeystoreProvider.java
@@ -6,6 +6,7 @@ import org.springframework.web.context.support.ServletContextResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -28,26 +29,27 @@ public class KeystoreProvider {
         String type = properties.getType();
         String password = properties.getPassword();
         Resource path = properties.getPath();
-        System.out.println("path: " + path);
-        System.out.println("path type: " + path.getClass());
 
-        // Create an instance of KeystoreResourceLoader
         KeystoreResourceLoader keystoreResourceLoader = new KeystoreResourceLoader();
 
         try {
             KeyStore keyStore = KeyStore.getInstance(type);
-            if (path instanceof ServletContextResource){
+
+            if (path instanceof ServletContextResource) {
                 String pathString = ((ServletContextResource) path).getPath().toString();
-                // Check if the content starts with "base64:"
                 if (pathString.startsWith("/base64:")) {
-                    // Use KeystoreResourceLoader to load the keystore from base64 content
-                    keyStore.load(keystoreResourceLoader.getResource(pathString.substring(1)).getInputStream(), password.toCharArray());
-                    return keyStore;
+                    // Load the keystore from base64 content
+                    try (InputStream inputStream = keystoreResourceLoader.getResource(pathString.substring(1)).getInputStream()) {
+                        keyStore.load(inputStream, password.toCharArray());
+                        return keyStore;
+                    }
                 }
             } else if (path != null && path.exists()) {
                 // Load the keystore from file
-                keyStore.load(new FileInputStream(path.getFile()), password.toCharArray());
-                return keyStore;
+                try (InputStream fileInputStream = new FileInputStream(path.getFile())) {
+                    keyStore.load(fileInputStream, password.toCharArray());
+                    return keyStore;
+                }
             } else {
                 throw new KeystoreProviderException("No keystore file found at " + path.getFile());
             }
@@ -60,6 +62,7 @@ public class KeystoreProvider {
         }
         return null;
     }
+
 
     public KeyStore getKeyStore() {
         return keystore;
