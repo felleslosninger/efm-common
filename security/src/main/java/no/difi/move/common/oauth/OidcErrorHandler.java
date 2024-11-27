b514044963b9d6865a3bb0bd13babd2ae2cc69a2
Brett Goreham <brettgoreham@gmail.com>
2024-11-27 09:34:54 +0100

    upgrade to spring boot 3.4.0

--- security/src/main/java/no/difi/move/common/oauth/OidcErrorHandler.java
+++ security/src/main/java/no/difi/move/common/oauth/OidcErrorHandler.java
@@ -1,9 +1,9 @@
 package no.difi.move.common.oauth;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.HttpClientErrorException;
@@ -17,20 +17,18 @@ import java.nio.charset.StandardCharsets;
 public class OidcErrorHandler extends DefaultResponseErrorHandler {
 
     @Override
-    protected void handleError(ClientHttpResponse response, HttpStatusCode statusCode) throws IOException {
-
-
+    public void handleError(ClientHttpResponse response) throws IOException {
         log.error("Response from token provider: " + IOUtils.toString(response.getBody(), StandardCharsets.UTF_8));
-        var status = HttpStatus.valueOf(statusCode.value());
+        var status = HttpStatus.valueOf(response.getStatusCode().value());
         switch (status.series()) {
             case CLIENT_ERROR:
-                throw new HttpClientErrorException(statusCode, response.getStatusText(),
+                throw new HttpClientErrorException(status, response.getStatusText(),
                         response.getHeaders(), getResponseBody(response), getCharset(response));
             case SERVER_ERROR:
-                throw new HttpServerErrorException(statusCode, response.getStatusText(),
+                throw new HttpServerErrorException(status, response.getStatusText(),
                         response.getHeaders(), getResponseBody(response), getCharset(response));
             default:
-                throw new RestClientException("Unknown status code [" + statusCode + "]");
+                throw new RestClientException("Unknown status code [" + status + "]");
         }
     }
 }
