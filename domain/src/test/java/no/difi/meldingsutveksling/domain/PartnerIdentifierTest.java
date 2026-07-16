891f6f5d2ffceb974b61d00f778f5d7bdec17096
Randi Eggum <randi.eggum@digdir.no>
2026-07-16 10:18:00 +0200

    MOVE-4750 Fjerne kode for DPFIO

--- domain/src/test/java/no/difi/meldingsutveksling/domain/PartnerIdentifierTest.java
+++ domain/src/test/java/no/difi/meldingsutveksling/domain/PartnerIdentifierTest.java
@@ -13,35 +13,30 @@ public class PartnerIdentifierTest {
         assertThat(PartnerIdentifier.parse("0192:987654321")).isEqualTo(Iso6523.of(ICD.NO_ORG, "987654321"));
         assertThat(PartnerIdentifier.parse("987654321")).isEqualTo(Iso6523.of(ICD.NO_ORG, "987654321"));
         assertThat(PartnerIdentifier.parse("08089409382")).isEqualTo(PersonIdentifier.parse("08089409382"));
-        assertThat(PartnerIdentifier.parse("0232c524-cb9b-4e9e-916d-318a5696184e")).isEqualTo(FiksIoIdentifier.parse("0232c524-cb9b-4e9e-916d-318a5696184e"));
         assertThat(PartnerIdentifier.parse("her-id:1234567")).isEqualTo(NhnIdentifier.parse("her-id:1234567"));
         assertThatThrownBy(() -> PartnerIdentifier.parse("A"))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("Unable to parse identifier = 'A'")
             .hasSuppressedException(new IllegalArgumentException("Invalid ISO6523 value: 'A'"))
-            .hasSuppressedException(new IllegalArgumentException("Invalid SSN='A'"))
-            .hasSuppressedException(new IllegalArgumentException("Invalid UUID string: A"));
+            .hasSuppressedException(new IllegalArgumentException("Invalid SSN='A'"));
     }
 
     @Test
     public void parseQualifiedIdentifier() {
         assertThat(PartnerIdentifier.parseQualifiedIdentifier("iso6523-actorid-upis::0192:987654321")).isEqualTo(Iso6523.of(ICD.NO_ORG, "987654321"));
         assertThat(PartnerIdentifier.parseQualifiedIdentifier("iso6523-actorid-upis::08089409382")).isEqualTo(PersonIdentifier.parse("08089409382"));
-        assertThat(PartnerIdentifier.parseQualifiedIdentifier("iso6523-actorid-upis::0232c524-cb9b-4e9e-916d-318a5696184e")).isEqualTo(FiksIoIdentifier.parse("0232c524-cb9b-4e9e-916d-318a5696184e"));
         assertThat(PartnerIdentifier.parseQualifiedIdentifier("nhn-actorid::her-id:1234567")).isEqualTo(NhnIdentifier.parse("her-id:1234567"));
         assertThatThrownBy(() -> PartnerIdentifier.parseQualifiedIdentifier("iso6523-actorid-upis::A"))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("Unable to parse identifier = 'iso6523-actorid-upis::A'")
             .hasSuppressedException(new IllegalArgumentException("Invalid ISO6523 value: 'A'"))
-            .hasSuppressedException(new IllegalArgumentException("Invalid SSN='A'"))
-            .hasSuppressedException(new IllegalArgumentException("Invalid UUID string: A"));
+            .hasSuppressedException(new IllegalArgumentException("Invalid SSN='A'"));
     }
 
     @Test
     public void isValid() {
         assertThat(PartnerIdentifier.isValid("0192:987654321")).isTrue();
         assertThat(PartnerIdentifier.isValid("08089409382")).isTrue();
-        assertThat(PartnerIdentifier.isValid("0232c524-cb9b-4e9e-916d-318a5696184e")).isTrue();
         assertThat(PartnerIdentifier.isValid("her-id:1234567")).isTrue();
         assertThat(PartnerIdentifier.isValid("A")).isFalse();
     }
@@ -50,7 +45,6 @@ public class PartnerIdentifierTest {
     public void isValidQualifiedIdentifier() {
         assertThat(PartnerIdentifier.isValidQualifiedIdentifier("iso6523-actorid-upis::0192:987654321")).isTrue();
         assertThat(PartnerIdentifier.isValidQualifiedIdentifier("iso6523-actorid-upis::08089409382")).isTrue();
-        assertThat(PartnerIdentifier.isValidQualifiedIdentifier("iso6523-actorid-upis::0232c524-cb9b-4e9e-916d-318a5696184e")).isTrue();
         assertThat(PartnerIdentifier.isValidQualifiedIdentifier("nhn-actorid::her-id:1234567")).isTrue();
         assertThat(PartnerIdentifier.isValidQualifiedIdentifier("iso6523-actorid-upis::A")).isFalse();
     }
