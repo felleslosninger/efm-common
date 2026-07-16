72e8672ac36402938cbac7982e836189634f98b4
Randi Eggum <randi.eggum@digdir.no>
2026-07-16 13:23:45 +0200

    MOVE-4750 Fjerne kode for DPFIO (#311)

--- domain/src/main/java/no/difi/meldingsutveksling/jackson/PartnerIdentifierModule.java
+++ domain/src/main/java/no/difi/meldingsutveksling/jackson/PartnerIdentifierModule.java
@@ -12,11 +12,9 @@ public class PartnerIdentifierModule extends SimpleModule {
 
         this.addSerializer(PartnerIdentifier.class, new PartnerIdentifierSerializer<>(PartnerIdentifier.class));
         this.addSerializer(Iso6523.class, new PartnerIdentifierSerializer<>(Iso6523.class));
-        this.addSerializer(FiksIoIdentifier.class, new PartnerIdentifierSerializer<>(FiksIoIdentifier.class));
         this.addSerializer(PersonIdentifier.class, new PartnerIdentifierSerializer<>(PersonIdentifier.class));
         this.addDeserializer(PartnerIdentifier.class, new PartnerIdentifierDeserializer<>(PartnerIdentifier.class, PartnerIdentifier::parse));
         this.addDeserializer(Iso6523.class, new PartnerIdentifierDeserializer<>(Iso6523.class, Iso6523::parse));
-        this.addDeserializer(FiksIoIdentifier.class, new PartnerIdentifierDeserializer<>(FiksIoIdentifier.class, FiksIoIdentifier::parse));
         this.addDeserializer(PersonIdentifier.class, new PartnerIdentifierDeserializer<>(PersonIdentifier.class, PersonIdentifier::parse));
         this.addDeserializer(NhnIdentifier.class, new PartnerIdentifierDeserializer<>(NhnIdentifier.class, NhnIdentifier::parse));
     }
